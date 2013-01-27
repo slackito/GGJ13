7e322c95ee68643d2ece2a080f468858cd5a60dd
fenderslender <a@b>
2013-01-27 17:29:48 +0100

    merge

---
+++ res/layer/enemies.js
@@ -0,0 +1,10 @@
+// sprite(from res/bg/),minSpacing,maxSpacing)
+[
+	["mujer1",3000,8000],
+	["mujer2",3000,8000],
+	["mujer3",3000,8000],
+	["monster1",3000,8000],
+	["monster2",3000,8000],
+	["monster3",3000,8000],
+
+]
