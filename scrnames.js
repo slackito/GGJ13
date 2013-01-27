a3a83ffa6e365941756ef34f654c54b42599b1a8
Rafael Gaitan <rafa.gaitan@mirage-tech.com>
2013-01-27 11:06:48 +0100

    added game over assets

--- scrnames.js
+++ scrnames.js
@@ -2,13 +2,14 @@ var scriptsnames = {
 "res/background/base.js" :
 // layer(from res/layer/),minDepth,maxDepth,speed)
 [
-    //["nubes",9,9,2],
-    //["montanyas",7,8,5],
-    //["edificios",5,6,50],
-    //["acera",3,3,400],
+    ["nubes",9,9,2],
+    ["montanyas",7,8,5],
+    ["edificios",5,6,50],
+    ["acera",3,3,400],
     ["farolalejos",2,2,400],
-    ["farolacerca",1,1,710],
-],
+    //["farolacerca",1,1,710],
+]
+,
 "res/layer/acera.js" :
 // sprite(from res/bg/),minSpacing,maxSpacing)
 [
